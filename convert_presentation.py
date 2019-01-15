e553775df1eda9be7660ed9fc2ef04bb84a50f57
Joe Public <joepublic@Josephs-Mini.fritz.box>
2019-01-15 14:24:14 +0000

    Working out if name needed to throw an exception

---
+++ convert_presentation.py
@@ -0,0 +1,7 @@
+def get_name(slide):
+    print slide
+    for shape in slide.shapes:
+      if shape.is_placeholder:
+         if shape.placeholder_format.idx == 0:
+             return shape.text
+    return "" #Not an exception because this is the *name*, which isn't needed for OBZ
