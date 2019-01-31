2adf0ae6c93aa47ff7856021c4da743db4e2a29c
Joe Reddington <joe@equalitytime.co.uk>
2019-01-31 12:17:30 +0000

    Basic 'is inside' method implemented

--- convert_presentation.py
+++ convert_presentation.py
@@ -1,6 +1,21 @@
+from pptx import Presentation
+
 def get_name(slide):
     for shape in slide.shapes:
       if shape.is_placeholder:
          if shape.placeholder_format.idx == 0:
              return shape.text
     return "" #Not an exception because this is the *name*, which isn't needed for OBZ
+
+
+def is_first_inside_second(first,second):
+    "Finds the centre the first shape and checks if it is in the bounds of the second"
+    #print "First  - top is {}, left is {}, height is {}, width is {}".format(first.top,first.left,first.height,first.width)
+    #print "Second - top is {}, left is {}, height is {}, width is {}".format(second.top,second.left,second.height,second.width)
+    y=first.top+(first.height/2)
+    x=first.left+(first.width/2)
+    #print "Middle of first is: {} from top and {} from left".format(y,x)
+    if second.top < y < second.top+second.height:
+        if second.left < x <second.left+second.width:
+            return True 
+    return False
--- test_convert_presentation.py
+++ test_convert_presentation.py
@@ -8,17 +8,28 @@ class TestConvertPresentation(unittest.TestCase):
 
     def test_read_number_of_slides(self):
         prs = Presentation("tests/testinputs/CK20V2.pptx")
-        self.assertEqual(len(prs.slides), 102)
+        self.assertEqual(len(prs.slides), 104)
 
     def test_name(self): 
         slide = Presentation("tests/testinputs/CK20V2.pptx").slides[0]
         self.assertEqual(convert_presentation.get_name(slide), u"Top page")
 
-    def test_name(self): 
+    def test_name2(self): 
         slide = Presentation("tests/testinputs/CK20V2.pptx").slides[2]
         self.assertEqual(convert_presentation.get_name(slide), u"")
 
-        
+    def test_inside(self): 
+        slide = Presentation("tests/testinputs/CK20V2.pptx").slides[3]
+        shapeA=slide.shapes[1]
+        shapeB=slide.shapes[0]
+        self.assertEqual(True,convert_presentation.is_first_inside_second(shapeA,shapeB))
+
+    def test_outside(self): 
+        slide = Presentation("tests/testinputs/CK20V2.pptx").slides[4]
+        shapeA=slide.shapes[1]
+        shapeB=slide.shapes[0]
+        self.assertEqual(False,convert_presentation.is_first_inside_second(shapeA,shapeB))
+ 
         
         
 #Todo - blank names 
